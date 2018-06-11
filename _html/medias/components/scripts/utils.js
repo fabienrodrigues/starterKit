8295778ecd4c484481cc64075e3f2cc3f5211e83
fabienrodrigues <f.rodrigues@peoleo.fr>
2018-06-11 17:50:21 +0200

    maj utils.js

--- _html/medias/components/scripts/utils.js
+++ _html/medias/components/scripts/utils.js
@@ -1,33 +1,53 @@
 
 function checkDevice() {
+    var $body = $('body');
     //CHECK DEVICE AND SET A CLASS TO THE BODY
-    var widthDevice = window.widthDevice();
-    $('body').removeClass('isMobile isTablet isDesktop isApple isAndroid isWindowsPhone isBlackberry no_touch');
+    $body.removeClass('isMobile isTablet isDesktop isApple isAndroid isWindowsPhone isBlackberry no-touch');
 
-    var deviceModel = null;
+    var deviceModel = getDeviceModel();
+    var deviceKind  = getDeviceKind();
+
+    for(var i in deviceModel) {
+        $body.addClass(deviceModel[i]);
+    }
+
+    $body.addClass(deviceKind);
+
+    if(!isTouchDevice() && !$body.hasClass('isAndroid') && !$body.hasClass('isIOS')) {
+        $body.addClass('no-touch');
+    }
+};
+
+
+function getDeviceKind() {
+    var widthDevice = window.widthDevice();
     var deviceKind = 'isDesktop';
 
-    if (navigator.userAgent.match(/iphone|ipod|ipad/i))             deviceModel = 'isApple';
-    if (navigator.userAgent.match(/Android/i))                      deviceModel = 'isAndroid';
-    if (navigator.userAgent.match(/iemobile/i))                     deviceModel = 'isWindowsPhone';
-    if (navigator.userAgent.match(/BlackBerry/i))                   deviceModel = 'isBlackberry';
-    if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1)  deviceModel = 'isFirefox';
-    if (window.chrome)                                              deviceModel = 'isChrome';
-
-    if (widthDevice <  768)                                         deviceKind = 'isMobile';
-    if (widthDevice >= 768 && widthDevice < 992)                    deviceKind = 'isTablet';
-    if (widthDevice >= 992)                                         deviceKind = 'isDesktop';
-
-    $('body').addClass(deviceModel).addClass(deviceKind);
-
-    if(!window.isTouchDevice()) { $('body').addClass('no_touch'); }
+    if (widthDevice <  768)                         deviceKind = 'isMobile';
+    if (widthDevice >= 768 && widthDevice < 992)    deviceKind = 'isTablet';
+    if (widthDevice >= 992)                         deviceKind = 'isDesktop';
 
     return deviceKind;
+};
+
+
+function getDeviceModel() {
+    var deviceModel = [];
+
+    if (navigator.userAgent.match(/iphone|ipod|ipad/i))             deviceModel.push('isIOS');
+    if (navigator.userAgent.match(/Android/i))                      deviceModel.push('isAndroid');
+    if (navigator.userAgent.match(/iemobile/i))                     deviceModel.push('isWindowsPhone');
+    if (navigator.userAgent.match(/BlackBerry/i))                   deviceModel.push('isBlackberry');
+    if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1)  deviceModel.push('isFirefox');
+    if (window.chrome)                                              deviceModel.push('isChrome');
+    if (navigator.userAgent.match(/Android/i) && window.chrome)     deviceModel.push('isAndroid');
+
+    return deviceModel;
 }
 
 
 function isTouchDevice() {
-    if('ontouchstart' in document.documentElement) {
+    if(window.DocumentTouch && document instanceof DocumentTouch) {
 
         return true;
 
@@ -36,14 +56,14 @@ function isTouchDevice() {
         return false;
         
     }
-}
+};
 
 
 function widthDevice() {
     return (window.innerWidth > 0) ? window.innerWidth : screen.width;
-}
+};
 
 
 function heightDevice() {
     return (window.innerHeight > 0) ? window.innerHeight : screen.height;
-}
+};
